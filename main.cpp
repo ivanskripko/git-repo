398d4cc6edb81439de5b54861952314c4a62cdc5
ivanskripko <scr.john@yandex.ru>
2015-12-02 21:32:17 +0300

    Added some new stuff.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main (int argc, const char* argv[]) 
 {
-	
-	std::cout << "Hello world!" << endl;
+	std::string name;
+	std::cin >> name;
+	std::cout << "Hello, " << name << "!" << std::endl;
 	
 	return 0;
 }
